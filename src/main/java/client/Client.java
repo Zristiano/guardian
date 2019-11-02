2ef88ec070eaff4339d7ce7438c9da1487e0b9a9
Zristiano <zengyuanmeng@gmail.com>
2019-11-02 15:49:20 -0500

    add salt in CountMinSketch

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -1,7 +1,19 @@
 package client;
 
+import com.google.common.hash.Hashing;
+
+import java.nio.charset.Charset;
+import java.util.UUID;
+
 public class Client {
     public static void main(String[] args) {
+        String uuid1 = UUID.randomUUID().toString();
+        String uuid2 = UUID.randomUUID().toString();
+        System.out.println(Hashing.sha256().hashString(uuid1, Charset.forName("UTF-8")).hashCode());
+        System.out.println(Hashing.sha256().hashString(uuid2, Charset.forName("UTF-8")).hashCode());
+        System.out.println(Hashing.sha256().hashString(uuid1, Charset.forName("UTF-8")).hashCode());
+        System.out.println(Hashing.sha256().hashString(uuid2, Charset.forName("UTF-8")).hashCode());
 
     }
+
 }
