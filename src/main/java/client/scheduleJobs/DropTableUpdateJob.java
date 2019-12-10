1c2d50cd79dceecc9b08e592d139ffb1bbb2a0c3
Zristiano <zengyuanmeng@gmail.com>
2019-12-09 20:36:28 -0600

    solve conflict

--- src/main/java/client/scheduleJobs/DropTableUpdateJob.java
+++ src/main/java/client/scheduleJobs/DropTableUpdateJob.java
@@ -10,21 +10,13 @@ import utils.GdLog;
 public class DropTableUpdateJob implements Job {
     @Override
     public void execute(JobExecutionContext context){
-<<<<<<< HEAD
 //        try {
 //            Thread.sleep(100);
 //        }catch (InterruptedException e){
 //            GdLog.e(""+e);
 //        }
         GdLog.i(this+"  DropTableUpdateJob is running");
-=======
-        try {
-            Thread.sleep(100);
-        }catch (InterruptedException e){
-            GdLog.e(""+e);
-        }
-//        GdLog.i("DropTableUpdateJob is running");
->>>>>>> 561627e937ae9bc224a080d08c47cfac3f031bd1
+
         Client client = Client.getInstance();
         client.getRateLimiter().syncDropTable(Constants.CLIENT_0, client.getBinder());
         GdLog.i(this+"  DropTableUpdateJob is finished");
--- src/main/java/client/scheduleJobs/FlatRequestJob.java
+++ src/main/java/client/scheduleJobs/FlatRequestJob.java
@@ -9,30 +9,18 @@ public class FlatRequestJob implements Job {
 
     @Override
     public void execute(JobExecutionContext context) {
-<<<<<<< HEAD
         GdLog.i(this+"  sending request at a steady pace");
-=======
-        GdLog.i("sending request at a steady pace");
->>>>>>> 561627e937ae9bc224a080d08c47cfac3f031bd1
         Client client = Client.getInstance();
         RequestGenerator requestGenerator = client.getRequestGenerator();
         RequestLogger requestLogger = client.getRequestLogger();
         RateLimiter rateLimiter = client.getRateLimiter();
-<<<<<<< HEAD
         for (int i=0; i<300000; i++){
-=======
-        for (int i=0; i<3000000; i++){
->>>>>>> 561627e937ae9bc224a080d08c47cfac3f031bd1
             Request request = new Request(requestGenerator.getRandomUser(10).getID());
             rateLimiter.request(request);
             requestLogger.log(request);
         }
         requestLogger.flush();
-
-<<<<<<< HEAD
         GdLog.i(this+"  finish sending request");
-=======
-        GdLog.i("finish sending request");
->>>>>>> 561627e937ae9bc224a080d08c47cfac3f031bd1
+
     }
 }
